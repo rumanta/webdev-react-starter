0b947bea8a67d6c334b6d76124559cbd278b237b
William Rumanta <williamrumanta@gmail.com>
2019-04-06 12:10:00 +0700

    Remove package-lock.json and update styled component on example starter component

--- src/components/ReactStarter/index.js
+++ src/components/ReactStarter/index.js
@@ -1,26 +1,28 @@
 import React, { Component } from "react";
 import logo from "../../logo.svg";
-import "./style.css";
+import { ReactStarterContainer } from "./style";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ReactStarterContainer>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </ReactStarterContainer>
     );
   }
 }
